72ccd7a2f3c86a75420d834a55c00d14d8d73080
abhimanyu121993 <softpromani@gmail.com>
2023-04-25 17:14:43 +0530

    main

--- resources/views/promotionform/login.blade.php
+++ resources/views/promotionform/login.blade.php
@@ -67,12 +67,12 @@ border-bottom-right-radius: .3rem;
               </div>
               <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
                 <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                  <h4 class="mb-4">Welcome to Portalfor Promotion of Teacher </h4>
+                  <h4 class="mb-4">Welcome to Portal for Promotion of Teacher </h4>
                   <p class="small mb-0">
                     <ul>
-                      <li>CAS-2018</li>
-                      <li>CAS-1998</li>
-                      <li>Notification (LNMU)</li>
+                      <li> <a href="Bihar CAS Promotion-2018_(2023).pdf "> CAS-2018 </a></li>
+                      <li> <a href="https://laws.gsbihar.online/statutes/statutes-48.html" target="_blank"> CAS-1998 </a></li>
+                      <li > Notification (LNMU)</li>
                     </ul>
                   </p>
                 </div>

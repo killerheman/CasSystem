c069218c9cb717294778f8f2a97c7c3bcaf39b3e
abhimanyu121993 <softpromani@gmail.com>
2023-04-30 22:26:41 +0530

    done

--- resources/views/promotionform/step10.blade.php
+++ resources/views/promotionform/step10.blade.php
@@ -72,7 +72,7 @@
                     <input type="text" class="total_claimed_score" disabled readonly id="total_value" value="0" required name="total_claimed_score" />
                 </div>
                 <div class="col-sm-9 d-flex justify-content-center">
-                    <button type="button" class="btn btn-warning mt-3 mb-3" data-toggle="modal" data-target="#docmodal"> View All Your Document</button>
+                    <button type="button" class="btn btn-warning mt-3 mb-3" id="docmodalbtn"> View All Your Document</button>
                 </div>
             </div>
             <div class="row g-3">
@@ -119,33 +119,29 @@
 
 
     {{-- Document Modal --}}
-    <div class="modal" id="docmodal" tabindex="-1"  aria-labelledby="docmodalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+
+    <div class="modal fade" id="dcmodal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
+              <h5 class="modal-title" id="exampleModalLabel">Your Uploaded Documents</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
               ...
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              {{-- <button type="button" class="btn btn-primary">Save changes</button> --}}
             </div>
           </div>
         </div>
       </div>
-
-
-
-
 @endsection
 
 
     @section('script')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script>
         $(document).ready(function() {
             var html = `<div class='col-md-3 parent1'><div class='row'><div class="col-sm-10 parent1">
@@ -177,5 +173,10 @@
 
             $('.total_claimed_score').val(score_sum >= 1 ? score_sum : 0);
         });
+    </script>
+    <script>
+          $(document).on('click','#docmodalbtn',function(){
+        $('#dcmodal').appendTo("body").modal('show');
+    })
     </script>
     @endsection

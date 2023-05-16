56186bb4c8f235e29cfde49415be8fc43eb91664
abhimanyu121993 <softpromani@gmail.com>
2023-05-17 00:56:02 +0530

    step 5 pre

--- resources/views/preview.blade.php
+++ resources/views/preview.blade.php
@@ -536,7 +536,7 @@
                         <th></th>
                     </tr>
                     @isset($users->step5)
-                        @foreach ($users->step5->where('type', 1) as $v)
+                        @foreach ($users->step5->where('type', 2) as $v)
                             <tr>
                                 <th scope="row">{{ $loop->index + 1 }}</th>
                                 <td>{{ $v->particulars ?? '' }}</td>
@@ -567,7 +567,7 @@
                 </thead>
                 <tbody>
                     @isset($users->step5)
-                        @foreach ($users->step5 as $v)
+                        @foreach ($users->step5->whereNot('type',2) as $v)
                             <tr>
                                 <th scope="row">{{ $loop->index + 1 }}</th>
                                 <td>{{ $v->particulars ?? '' }}</td>
